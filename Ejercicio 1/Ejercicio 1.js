4b62e4f2f5ed73e09ffa026d92206838ce1fa4d5
AlbertoDelPozo <72515390+AlbertoDelPozo@users.noreply.github.com>
2022-01-17 19:37:43 +0100

    Ejercicio 1 completado
    Ejercicio 1 completado

--- Ejercicio 1/Ejercicio 1.js
+++ Ejercicio 1/Ejercicio 1.js
@@ -1,18 +1,53 @@
-var contador = 0;
-var timer;
-
+//main
 function inicio() {
-    if (contador == 0) {
-        timer = setTimeout(suma, 1000);        
-    } 
-    if (contador == 4) {
-        clearTimeout(timer);
-    }
-}
+    //variable para almacenar todos los divs
+    var divs = document.querySelectorAll("div.num");
+    // console.log(divs);
+    // array con los ids generados por el setTimeout
+    var ids = [];
 
-function suma() {
-    contador++;
-    
-}
+    // función para mostrar los elementos ocultos
+    function mostrar() {
+        //hacemos un forEach para establecer los setTimeout
+        divs.forEach((ele, index) => {
+            //setTimeout
+            let id = setTimeout(() => {
+                //mostramos el div con display block
+                ele.style.display = "block";
+            }, 1000*index);
+            //introducimos en el array general los id de cada timeout
+            ids.push(id);
+        });
+    }
+
+    //ejecutamos la función
+    mostrar();
+
+    //cada vez que hagamos click en un div se ocultarán los divs de nuevo
+    divs.forEach((ele) => {
+        ele.addEventListener("click", () => {
+            ids.forEach((divActual) => {
+                // finalizamos los timeout
+                clearTimeout(divActual);
+            });
+
+            // reiniciamos los divs
+            reiniciar();
+        })
+    });
+
+    //función para reiniciar los divs
+    function reiniciar() {
+        // vaciamos ell array de ids
+        ids = [];
+        // forEacha para ocultar los divs
+        divs.forEach((divActual) => {
+            // ocultamos los divs
+            divActual.style.display = "none";
+        })
+        // volvemos a ejecutar la función para mostrar los divs de nuevo
+        mostrar();
+    }
+};
 
 window.onload = inicio;
