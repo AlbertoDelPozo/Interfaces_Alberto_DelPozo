16cdf7d1da07dffc5c00e1228ab641af6001ba4d
AlbertoDelPozo <72515390+AlbertoDelPozo@users.noreply.github.com>
2022-01-22 19:33:02 +0100

    avance en los ejercicios

---
+++ Ejercicio 3/colisiones.js
@@ -0,0 +1,6 @@
+function moveRight() {
+    var direction = document.getElementById('right');
+    var movePX = document.getElementById('mov').value;
+    console.log(direction,movePX);
+    // direction.style.transition = ;
+}
