e55ae20386fe6fc4bb8c6b7cc0bcfcb5ff53748a
AlbertoDelPozo <72515390+AlbertoDelPozo@users.noreply.github.com>
2022-02-13 11:58:30 +0100

    Ejercicio 1 completado

--- WebComponents/Ejercicio 1/blink.js
+++ WebComponents/Ejercicio 1/blink.js
@@ -1,35 +1,17 @@
 class Blink extends HTMLElement {
-    constructor() {
+    constructor () {
         super ();
     }
 
     connectedCallback() {
-        let intervalo = 1000;
-        let baseColor = "inherit";
-        let alternativeColor = "transparent";
+        const intervalo = (this.getAttribute ('changeinterval') || 1)*1000;
+        const base = this.getAttribute ('basecolor') || 'inherit';
+        const alternative = this.getAttribute ('alternativecolor') || 'transparent';
         let num = 0;
 
-        if (this.getAttribute('changeinterval') != null) {
-            intervalo = this.getAttribute('changeinterval')*1000;
-        }
-
-        if (this.getAttribute('basecolor') != null) {
-            baseColor = this.getAttribute('basecolor');
-        }
-
-        if (this.getAttribute('alternativecolor') != null) {
-            alternativeColor = this.getAttribute('alternativecolor');
-        }
-
         setInterval(() => {
-            ++num;
-            if (n%2 != 0) {
-                this.style.color = alternativeColor;
-            } else {
-                this.style.color = base;
-            }
-
-        }, intervalo);
+            this.style.color = ++num % 2 ? alternative : base;
+        }, intervalo)
     }
 }
 customElements.define('wc-blink', Blink);
