dac37a082152e6b5367911566a0578bf2678efb2
AlbertoDelPozo <72515390+AlbertoDelPozo@users.noreply.github.com>
2022-02-13 16:19:41 +0100

    Ejercicio 2 completado

--- WebComponents/Ejercicio 2/blink2.js
+++ WebComponents/Ejercicio 2/blink2.js
@@ -8,5 +8,37 @@ class BlinkSpan extends HTMLSpanElement {
         const base = this.basecolor || 'inherit';
         const alternative = this.alternativecolor ||'transparent';
         let num = 0;
+
+        setInterval(() => {
+            this.style.color = ++num % 2 ? alternative : base;
+        }, intervalo);
+    }
+
+    // getters y setters
+
+    get changeinterval() {
+        return this.getAttribute('changeinterval');
+    }
+
+    get basecolor() {
+        return this.getAttribute('basecolor');
+    }
+
+    get alternativecolor() {
+        return this.getAttribute('alternativecolor');
+    }
+
+    set changeinterval(value) {
+        this.setAttribute('changeinterval', value);
+    }
+
+    set basecolor(value) {
+        this.setAttribute('basecolor', value);
+    }
+
+    set alternativecolor(value) {
+        this.setAttribute('alternativecolor', value);
     }
 }
+
+customElements.define ('wc-blink-span', BlinkSpan, { extends: 'span' });
