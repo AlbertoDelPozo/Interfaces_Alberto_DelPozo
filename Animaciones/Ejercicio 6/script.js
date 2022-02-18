a08082b67c78da387c87925809ec8398e34e963b
AlbertoDelPozo <72515390+AlbertoDelPozo@users.noreply.github.com>
2022-02-18 11:48:55 +0100

    Ejercicio 6 completado

--- Animaciones/Ejercicio 6/script.js
+++ Animaciones/Ejercicio 6/script.js
@@ -1,10 +1,59 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // en primer lugar para que cargue un fichero debemos hacer un fetch normal
-    const ficheroUno = fetch('https://reqres.in/api/users');
-    const ficheroDos = fetch('https://reqres.in/api/login');
-    console.log(ficheroUno);
-    console.log(ficheroDos);
-    // para los dos ficheros debemos poner un fetch all
+function cargarBarra() {
+    let barra = document.querySelector(".barra");
+    let tiempo = 2000;
+    let proceso = 0;
+    var start = null;
+    
+    return new Promise((resove, reject) => {
+        function step(timestamp) {
+            if (!start) {
+                start = timestamp;
+            }
+            var progress = timestamp - start;
+            let pct = ((progress*100)/tiempo) + "%";
+            barra.style.width = pct;
 
-    // finalmente para el más rápido 
+            if (progress > tiempo) {
+                proceso = window.requestAnimationFrame(step);
+            } else {
+                barra.style.width = "100%";
+                resove("El fichero cargó bien");
+            }
+        }
+
+        proceso = window.requestAnimationFrame(step);
+    });
+}
+
+function cargarPromesa(opcion){
+    if (opcion === "all"){
+		return Promise.all([fetch('https://reqres.in/api/users'),fetch('https://reqres.in/api/login')]);
+	} else if (opcion === "race"){
+		return Promise.race([fetch('https://reqres.in/api/users'),fetch('https://reqres.in/api/login')]);
+	} else {
+		return fetch('https://reqres.in/api/users');
+	}
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let mensaje = document.getElementById('mensaje');
+
+    document.getElementById("cargar").addEventListener("click", () => {
+        mensaje.textContent = "Cargando";
+
+        cargarPromesa(document.getElementById("modo").value)
+        .then( (respuesta) => {
+            console.log(respuesta);
+            return cargarBarra();
+        })
+
+        .then( (respuesta) => {
+            mensaje.textContent = respuesta;
+        })
+
+        .catch( (error)  => {
+            mensaje.textContent = "Error " + error;
+            document.querySelector(".barra").style.width = "0%";
+        });
+    });
 })
